3d4732899cbade9da3c005a32aca969b908e5fd4
Sanxom <masonbell1303@gmail.com>
2025-12-09 16:03:56 -0600

    Initial commit

---
+++ Assets/_Project/Scripts/Utils/SingletonMonobehaviour.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public abstract class SingletonMonobehaviour<T> : MonoBehaviour where T: MonoBehaviour
+{
+    public static T Instance { get; protected set; }
+
+    protected virtual void Awake()
+    {
+        if (Instance == null)
+            Instance = this as T;
+        else
+            Destroy(gameObject);
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# FarmingRPG
+A feature-rich Farming RPG made from scratch in Unity by me.
